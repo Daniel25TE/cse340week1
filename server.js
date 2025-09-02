abb2c7bbb60dcf98044e365df3bd0ef6bb7ab0e3
Daniel25TE <danielalejandrosud25@gmail.com>
2025-09-02 17:55:56 -0600

    maybe

--- server.js
+++ server.js
@@ -6,25 +6,34 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", {title: "Homes"})
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on port ${port}`)
 })
+
